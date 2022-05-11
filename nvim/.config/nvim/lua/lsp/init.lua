5388ff9e291e0afc172e19982c9423e299c94920
Eric <cowboy-bebug@users.noreply.github.com>
2022-05-12 09:38:21 +1200

    feat(nvim): add ansiblels

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -42,6 +42,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
 for _, lsp in pairs({
+    "ansiblels",
     "bashls",
     "dockerls",
     "gopls",
