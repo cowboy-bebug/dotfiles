02691845593753b0be74d39930a0d927db19aa17
Eric <cowboy-bebug@users.noreply.github.com>
2022-06-30 10:27:31 +1200

    style(nvim): correct whitespace

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -2,8 +2,8 @@
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local map = vim.api.nvim_set_keymap
 local opts = {
-    noremap = true,
-    silent = true,
+  noremap = true,
+  silent = true,
 }
 
 map("n", "<space>of", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
@@ -14,58 +14,58 @@ map("n", "<space>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-    -- Mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local buf_map = vim.api.nvim_buf_set_keymap
-    buf_map(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-    buf_map(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-    buf_map(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-    buf_map(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-    buf_map(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-    buf_map(bufnr, "n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-    buf_map(bufnr, "n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-    buf_map(bufnr, "n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-    buf_map(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-    buf_map(bufnr, "n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local buf_map = vim.api.nvim_buf_set_keymap
+  buf_map(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+  buf_map(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  buf_map(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  buf_map(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+  buf_map(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+  buf_map(bufnr, "n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+  buf_map(bufnr, "n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+  buf_map(bufnr, "n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+  buf_map(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+  buf_map(bufnr, "n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
 
-    -- hack: disable diagnostic for helm files
-    if vim.bo[bufnr].buftype ~= "" or vim.bo[bufnr].filetype == "helm" then
-        vim.diagnostic.disable(bufnr)
-        vim.defer_fn(function() vim.diagnostic.reset(nil, bufnr) end, 1000)
-    end
+  -- hack: disable diagnostic for helm files
+  if vim.bo[bufnr].buftype ~= "" or vim.bo[bufnr].filetype == "helm" then
+    vim.diagnostic.disable(bufnr)
+    vim.defer_fn(function() vim.diagnostic.reset(nil, bufnr) end, 1000)
+  end
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
 for _, lsp in pairs({
-    "ansiblels",
-    "bashls",
-    "dockerls",
-    "gopls",
-    "pyright",
-    "rust_analyzer",
-    "taplo",
-    "terraformls",
-    "yamlls",
+  "ansiblels",
+  "bashls",
+  "dockerls",
+  "gopls",
+  "pyright",
+  "rust_analyzer",
+  "taplo",
+  "terraformls",
+  "yamlls",
 }) do
-    require("lspconfig")[lsp].setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-        flags = {
-            debounce_text_changes = 150,
+  require("lspconfig")[lsp].setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    flags = {
+      debounce_text_changes = 150,
+    },
+    settings = {
+      ["rust-analyzer"] = {
+        cargo = {
+          allFeatures = true,
         },
-        settings = {
-            ["rust-analyzer"] = {
-                cargo = {
-                    allFeatures = true,
-                },
-            },
-        },
-    })
+      },
+    },
+  })
 end
 
 require("lsp/cmp")
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,53 +1,53 @@
 local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-    packer_bootstrap = vim.fn.system({
-        "git",
-        "clone",
-        "--depth",
-        "1",
-        "https://github.com/wbthomason/packer.nvim",
-        install_path,
-    })
+  packer_bootstrap = vim.fn.system({
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  })
 end
 
 require("packer").startup(function(use)
-    use "wbthomason/packer.nvim"
-    use "navarasu/onedark.nvim"
+  use "wbthomason/packer.nvim"
+  use "navarasu/onedark.nvim"
 
-    use "kyazdani42/nvim-web-devicons"
-    use "kyazdani42/nvim-tree.lua"
-    use "nvim-lualine/lualine.nvim"
-    use "romgrk/barbar.nvim"
+  use "kyazdani42/nvim-web-devicons"
+  use "kyazdani42/nvim-tree.lua"
+  use "nvim-lualine/lualine.nvim"
+  use "romgrk/barbar.nvim"
 
-    use {
-        "nvim-treesitter/nvim-treesitter",
-        run = ":TSUpdate",
-    }
-    use "nvim-treesitter/nvim-treesitter-refactor"
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+  }
+  use "nvim-treesitter/nvim-treesitter-refactor"
 
-    use "nvim-lua/plenary.nvim"
-    use "nvim-telescope/telescope.nvim"
-    use {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        run = "make",
-    }
-    use "lewis6991/gitsigns.nvim"
+  use "nvim-lua/plenary.nvim"
+  use "nvim-telescope/telescope.nvim"
+  use {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    run = "make",
+  }
+  use "lewis6991/gitsigns.nvim"
 
-    use "neovim/nvim-lspconfig" -- Collection of configurations for built-in LSP client
-    use "hrsh7th/nvim-cmp" -- Autocompletion plugin
-    use "hrsh7th/cmp-nvim-lsp" -- LSP source for nvim-cmp
-    use "saadparwaiz1/cmp_luasnip" -- Snippets source for nvim-cmp
-    use "L3MON4D3/LuaSnip" -- Snippets plugin
-    use "onsails/lspkind-nvim"
-    use "jose-elias-alvarez/null-ls.nvim"
-    use "williamboman/nvim-lsp-installer"
+  use "neovim/nvim-lspconfig" -- Collection of configurations for built-in LSP client
+  use "hrsh7th/nvim-cmp" -- Autocompletion plugin
+  use "hrsh7th/cmp-nvim-lsp" -- LSP source for nvim-cmp
+  use "saadparwaiz1/cmp_luasnip" -- Snippets source for nvim-cmp
+  use "L3MON4D3/LuaSnip" -- Snippets plugin
+  use "onsails/lspkind-nvim"
+  use "jose-elias-alvarez/null-ls.nvim"
+  use "williamboman/nvim-lsp-installer"
 
-    use "numToStr/Comment.nvim"
-    use "towolf/vim-helm"
-    use "norcalli/nvim-colorizer.lua"
+  use "numToStr/Comment.nvim"
+  use "towolf/vim-helm"
+  use "norcalli/nvim-colorizer.lua"
 
-    if packer_bootstrap then require("packer").sync() end
+  if packer_bootstrap then require("packer").sync() end
 end)
 
 require("plugins/barbar")
