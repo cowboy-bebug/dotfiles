1368c692c8355831ba3e081a09d385acca9ea153
Eric <cowboy-bebug@users.noreply.github.com>
2022-06-30 10:29:00 +1200

    feat(nvim): remove obsolete tree settings

--- nvim/.config/nvim/lua/plugins/tree/init.lua
+++ nvim/.config/nvim/lua/plugins/tree/init.lua
@@ -1,27 +1,15 @@
 local opts = {
-    noremap = true,
-    silent = true,
+  noremap = true,
+  silent = true,
 }
 
 vim.api.nvim_set_keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
 
-vim.g.nvim_tree_icons = {
-    git = {
-        unstaged = "✗",
-        staged = "✓",
-        unmerged = "",
-        renamed = "➜",
-        untracked = "★",
-        deleted = "",
-        ignored = "◌",
-    },
-}
-
 require("nvim-tree").setup({
-    diagnostics = { enable = true },
-    filters = {
-        dotfiles = false,
-        custom = { "^.git$" },
-    },
-    git = { ignore = false },
+  diagnostics = { enable = true },
+  filters = {
+    dotfiles = false,
+    custom = { "^.git$" },
+  },
+  git = { ignore = false },
 })
