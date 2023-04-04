2a4818e9e811536df8579fa8aa9631b2a9d9ddd9
Eric <cowboy-bebug@users.noreply.github.com>
2023-04-04 16:46:57 +1200

    feat(nvim): remove deprecated bufferline

--- nvim/.config/nvim/lua/plugins/barbar/init.lua
+++ nvim/.config/nvim/lua/plugins/barbar/init.lua
@@ -8,11 +8,3 @@ map("n", "<C-h>", ":BufferPrevious<CR>", opts)
 map("n", "<C-l>", ":BufferNext<CR>", opts)
 map("n", "<C-p>", ":BufferPick<CR>", opts)
 map("n", "<C-c>", ":BufferClose<CR>", opts)
-
-vim.g.bufferline = {
-    animation = true,
-    closable = true,
-    clickable = true,
-    icon_separator_active = "",
-    icon_separator_inactive = "",
-}
