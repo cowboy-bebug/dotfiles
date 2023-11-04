9e76537b1dd8a10d86421f6ef529d46223ca8aaf
Eric <cowboy-bebug@users.noreply.github.com>
2023-11-04 14:41:54 +1300

    feat(doom): change org-directory from home

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -41,7 +41,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/github.com/cowboy-bebug/org")
 (after! org
   (setq org-hide-emphasis-markers t))
 
