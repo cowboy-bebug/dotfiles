51b688b2514ad7d7ab534fcfd18b5944152807e1
Eric Lim <cowboy-bebug@users.noreply.github.com>
2023-11-10 21:09:58 +1300

    feat(doom): add literal config with tidyups

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -1,51 +1,21 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
-;; (setq user-full-name "John Doe"
-;;       user-mail-address "john@doe.com")
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-symbol-font' -- for symbols
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-one)
-(setq doom-font (font-spec :family "Roboto Mono" :size 14 :weight 'medium))
 
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq doom-font
+      (font-spec
+       :family "Roboto Mono"
+       :size 14
+       :weight 'medium))
+(setq doom-variable-pitch-font
+      (font-spec
+       :family "Georgia"
+       :size 20))
+
 (setq display-line-numbers-type 'relative)
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/github.com/cowboy-bebug/org")
 (after! org
   (setq org-edit-src-content-indentation nil)
   (setq org-hide-emphasis-markers t)
-  (setq org-support-shift-select t)
   (use-package! org-superstar
     :hook
     (org-mode . org-superstar-mode))
@@ -56,48 +26,11 @@
     :hook
     (org-mode . org-auto-tangle-mode)))
 
+(setq auto-mode-alist
+      (append '(("\\.mdx\\'" . markdown-mode))
+              auto-mode-alist))
 
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
-;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-
-(use-package! projectile
-  :config
-  (setq projectile-track-known-projects-automatically nil))
-
-(setq-hook! 'js-mode-hook +format-with-lsp nil)
-(setq-hook! 'js-mode-hook +format-with :none)
-(add-hook 'js-mode-hook 'prettier-js-mode)
-
-(use-package! flyspell
+(after! flyspell
   :config
   (let ((aspell-personal-directory
          (string-trim-right
@@ -105,30 +38,22 @@
     (setq ispell-personal-dictionary
           (expand-file-name ".aspell.en.pws"  aspell-personal-directory))))
 
-(use-package! markdown-mode
-  :config
-  (setq markdown-command "markdown")
-  (add-hook 'markdown-mode-hook
-            (lambda()
-              (seq fill-column 80)
-              (prettier-js-mode 1)))
-  :mode
-  ("\\.mdx\\'" . markdown-mode))
-
-(use-package! typescript-mode
-  :config
-  (setq typescript-indent-level 2))
-
 (use-package! nov
-  :mode
-  ("\\.epub\\'" . nov-mode))
+  :mode ("\\.epub\\'" . nov-mode))
 
-(use-package! elfeed
+(after! elfeed
   :config
   (setq elfeed-search-filter "@1-month-ago +unread")
-  (setq rmh-elfeed-org-files (list (expand-file-name "./elfeed.org" doom-user-dir)))
+  (setq elfeed-feeds
+        '(("https://feeds.feedburner.com/TheDailyWtf" programming)
+          ("https://lobste.rs/rss" programming)
+          ("https://www.news.ycombinator.com/rss" programming)
+          ("https://www.reddit.com/r/programming/.rss" programming)
+          ("https://techcrunch.com/feed" new tech)))
   (setq browse-url-browser-function 'eww-browse-url)
   (set-popup-rules!
     '(("^\\*eww\\*"
-       ;; :side right :slot 1 :size 0.65 :select t))))
-       :side right :slot 1 :size #'+popup-shrink-to-fit :select t))))
+       :side right
+       :slot 1
+       :size #'+popup-shrink-to-fit
+       :select t))))
