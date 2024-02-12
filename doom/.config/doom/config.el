983a561ee026228bce3731112c3ad164d98fb303
Eric Lim <cowboy-bebug@users.noreply.github.com>
2024-02-12 19:55:01 +1300

    feat(doom): update theme

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -2,28 +2,44 @@
 ;;; For more info, see `~/.config/emacs/templates/config.example.el'
 
 ;; ui
-(setq doom-theme 'tango
-      doom-font                (font-spec :family "Hack Nerd Font" :size 12)
+(setq doom-theme 'doom-one-light
+      doom-font                (font-spec :family "Menlo" :size 14)
       doom-variable-pitch-font (font-spec :family "Georgia"        :size 20)
       doom-modeline-vcs-max-length 40 ;; to display branch name
       display-line-numbers-type 'relative)
 (remove-hook '+doom-dashboard-functions #'doom-dashboard-widget-footer)
 
 (when (display-graphic-p)
-  (when (eq doom-theme 'tango)
+  (when (eq doom-theme 'doom-earl-grey)
+    (custom-set-faces! '(font-lock-keyword-face :weight bold)))
+
+  (when (eq doom-theme 'doom-homage-white)
+    (custom-set-faces! '(font-lock-type-face :slant italic)))
+
+  (when (member doom-theme '(adwaita tango))
     (let ((current-line-background "#D6D6D6")
           (comment-foreground      "#9E9A95")
           (dired-dir-foreground    "#204A87"))
+      (custom-theme-set-faces! 'adwaita
+        '(hl-line                  :background "#DAE5f1")
+        '(line-number-current-line :background "#DAE5f1")
+        `(diredfl-dir-heading      :background nil)
+        `(diredfl-dir-name         :background nil)
+        `(diredfl-dir-priv         :background nil)
+        '(diredfl-no-priv          :background nil)
+        '(diredfl-exec-priv        :background nil)
+        '(diredfl-read-priv        :background nil)
+        '(diredfl-write-priv       :background nil))
       (custom-theme-set-faces! 'tango
         `(hl-line                  :background ,current-line-background)
         `(line-number-current-line :background ,current-line-background)
-        `(diredfl-dir-heading :background nil :foreground ,dired-dir-foreground :weight bold)
-        `(diredfl-dir-name    :background nil :foreground ,dired-dir-foreground)
-        `(diredfl-dir-priv    :background nil :foreground ,dired-dir-foreground)
-        '(diredfl-no-priv     :background nil)
-        '(diredfl-exec-priv   :background nil)
-        '(diredfl-read-priv   :background nil)
-        '(diredfl-write-priv  :background nil))
+        `(diredfl-dir-heading      :background nil :foreground ,dired-dir-foreground :weight bold)
+        `(diredfl-dir-name         :background nil :foreground ,dired-dir-foreground)
+        `(diredfl-dir-priv         :background nil :foreground ,dired-dir-foreground)
+        '(diredfl-no-priv          :background nil)
+        '(diredfl-exec-priv        :background nil)
+        '(diredfl-read-priv        :background nil)
+        '(diredfl-write-priv       :background nil))
       (custom-set-faces!
         `(font-lock-comment-face :foreground ,comment-foreground)
         `(font-lock-doc-face     :foreground ,comment-foreground)))))
@@ -33,13 +49,13 @@
   (setq ispell-dictionary "en"
         spell-fu-word-delimit-camel-case t))
 
-;; org
+;; org / markdown
 (setq org-directory "~/org/")
 (after! org
   (setq
+   markdown-hide-markup t
    ;; org-edit-src-content-indentation nil
-   org-hide-emphasis-markers t)
-  )
+   org-hide-emphasis-markers t))
 
 ;; preloaded packages
 (after! magit
