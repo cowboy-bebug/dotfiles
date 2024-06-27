2834d0d28842db7fba21be2e826a9d77c3ae35b8
Eric <cowboy-bebug@users.noreply.github.com>
2024-06-27 13:19:18 +1200

    feat(doom): remove theme customisation

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -9,41 +9,6 @@
       display-line-numbers-type 'relative)
 (remove-hook '+doom-dashboard-functions #'doom-dashboard-widget-footer)
 
-(when (display-graphic-p)
-  (when (member doom-theme '(doom-earl-grey doom-plain))
-    (custom-set-faces! '(font-lock-keyword-face :weight bold)))
-
-  (when (eq doom-theme 'doom-homage-white)
-    (custom-set-faces! '(font-lock-type-face :slant italic)))
-
-  (when (member doom-theme '(adwaita tango))
-    (let ((current-line-background "#D6D6D6")
-          (comment-foreground      "#9E9A95")
-          (dired-dir-foreground    "#204A87"))
-      (custom-theme-set-faces! 'adwaita
-        '(hl-line                  :background "#DAE5f1")
-        '(line-number-current-line :background "#DAE5f1")
-        `(diredfl-dir-heading      :background nil)
-        `(diredfl-dir-name         :background nil)
-        `(diredfl-dir-priv         :background nil)
-        '(diredfl-no-priv          :background nil)
-        '(diredfl-exec-priv        :background nil)
-        '(diredfl-read-priv        :background nil)
-        '(diredfl-write-priv       :background nil))
-      (custom-theme-set-faces! 'tango
-        `(hl-line                  :background ,current-line-background)
-        `(line-number-current-line :background ,current-line-background)
-        `(diredfl-dir-heading      :background nil :foreground ,dired-dir-foreground :weight bold)
-        `(diredfl-dir-name         :background nil :foreground ,dired-dir-foreground)
-        `(diredfl-dir-priv         :background nil :foreground ,dired-dir-foreground)
-        '(diredfl-no-priv          :background nil)
-        '(diredfl-exec-priv        :background nil)
-        '(diredfl-read-priv        :background nil)
-        '(diredfl-write-priv       :background nil))
-      (custom-set-faces!
-        `(font-lock-comment-face :foreground ,comment-foreground)
-        `(font-lock-doc-face     :foreground ,comment-foreground)))))
-
 ;; recent files
 (after! recentf
   (add-to-list 'recentf-exclude (recentf-expand-file-name "~/.config/emacs/.local/.*")))
