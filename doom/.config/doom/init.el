51f40099995eadbe5aae7016e5e47e595289ce24
Eric Lim <cowboy-bebug@users.noreply.github.com>
2025-01-30 21:41:54 +1300

    feat(doom): enable org-crypt

--- doom/.config/doom/init.el
+++ doom/.config/doom/init.el
@@ -154,6 +154,7 @@
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org              ; organize your plain life in plain text
+        +crypt
         +pandoc
         +roam2)
        ;;php               ; perl's insecure younger brother
