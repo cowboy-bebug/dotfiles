c5a268eafef0a5e6655551869fe84d8245866ec9
Eric Lim <cowboy-bebug@users.noreply.github.com>
2025-08-09 16:53:17 +1200

    feat(doom): show me all the org

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -100,7 +100,7 @@
         org-log-into-drawer t
         org-pretty-entities t
         org-src-preserve-indentation nil
-        org-startup-folded t
+        org-startup-folded nil
         org-startup-with-inline-images t
         org-startup-with-latex-preview t
         org-tags-column 80))
